bc21c916a0140143bcb49231a63be0acf2c23c23
belsonselva <belsonselva@gmail.com>
2023-04-16 21:14:50 +0530

    new commit

---
+++ src/components/Home/Cancerspecialties.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cancerspecialties() {
+  return (
+    <div className='servcies-area gray-bg pt-115 pb-90'>
+
+    </div>
+  )
+}
+
+export default Cancerspecialties
